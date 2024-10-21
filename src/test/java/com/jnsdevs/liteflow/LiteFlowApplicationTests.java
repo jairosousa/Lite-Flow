5d8ca4d443538d5e33ec25143e1c90e050f5345f
Jairo Nascimento <jaironsousa@gmail.com>
2024-10-21 09:11:32 -0300

    Mudança do gerenciador de pacotes

--- src/test/java/com/jnsdevs/liteflow/LiteFlowApplicationTests.java
+++ src/test/java/com/jnsdevs/liteflow/LiteFlowApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class LiteFlowApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+	@Test
+	void contextLoads() {
+	}
 
 }
